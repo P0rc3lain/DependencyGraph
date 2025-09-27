38d1aa28066379509aec7e9211a8c40d63127d45
Mateusz Stompór <mateusz.stompor@gmail.com>
2025-09-27 15:50:34 +0200

    Enhance multithreaded visitor

--- Sources/PNDependencyGraph/Visitors/PNMultithreadVisitor.swift
+++ Sources/PNDependencyGraph/Visitors/PNMultithreadVisitor.swift
@@ -5,15 +5,20 @@ public final class PNMultithreadVisitor {
     private let queue = OperationQueue()
     public init(graph newGraph: PNCompiledGraph) {
         graph = newGraph
-        queue.maxConcurrentOperationCount = graph.nodes.count
+        queue.qualityOfService = .userInitiated
+        queue.maxConcurrentOperationCount = graph.nodes.count - 1
     }
     
-    public func visit(completion: @escaping @Sendable (PNNode) -> Void) {
-        for node in graph.nodes {
+    public func visit(completion: @Sendable @escaping (PNNode) -> Void) {
+        guard let first = graph.nodes.first else {
+            return
+        }
+        for node in graph.nodes.dropFirst() {
             queue.addOperation {
                 completion(node)
             }
         }
+        completion(first)
         queue.waitUntilAllOperationsAreFinished()
     }
 }
